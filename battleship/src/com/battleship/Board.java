ac9715920a2729f669e52214c35bc120eb5abc2c
Rafael Lima <rafaelimadev@gmail.com>
2021-12-16 16:16:46 -0300

    board class implemented

---
+++ battleship/src/com/battleship/Board.java
@@ -0,0 +1,52 @@
+package com.battleship;
+
+import java.util.Arrays;
+
+public class Board {
+
+    private char[][] board = new char[10][10];
+
+
+    public void buildBoard(){
+        for(int i = 0; i < board.length; i++){
+            Arrays.fill(board[i], ' ');
+        }
+    }
+
+    public void showBoard(){
+        char letters = 'A';
+
+        System.out.println("---------------------------------------------");
+        System.out.println("                  JOGADOR                    ");
+        System.out.println("---------------------------------------------");
+        System.out.println("|   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |");
+        for (int i = 0; i < board.length; i++){
+            System.out.printf("| %c |", letters);
+            letters++;
+            for(int j = 0; j < board[i].length; j++){
+                System.out.printf(" %c |", board[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("---------------------------------------------");
+        System.out.println(" ");
+    }
+
+    public void setBoard(char row, int collumn, char value){
+
+
+        int rowInt = transformLetterInNumber(row);
+        board[rowInt][collumn] = value;
+    }
+
+    public char getPositionValue(char row, int collumn){
+        int rowInt = transformLetterInNumber(row);
+        return board[rowInt][collumn];
+    }
+
+    public int transformLetterInNumber(char row){
+        //Gambiarra que transforma um char em numero.
+        return Character.toLowerCase(row) - 'a';
+    }
+
+}
--- battleship/src/com/battleship/Main.java
+++ battleship/src/com/battleship/Main.java
@@ -2,4 +2,15 @@ package com.battleship;
 
 public class Main {
 
+    public static void main(String[] args) {
+
+        //As linha abaixo são só para teste das funcionalidades.
+        Board player = new Board();
+        player.buildBoard();
+        player.showBoard();
+        player.setBoard('e', 5, '-');
+        player.showBoard();
+        System.out.println(player.getPositionValue('e', 5));
+    }
+
 }
