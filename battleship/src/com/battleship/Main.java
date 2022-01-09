4272a9d362bb1c4a3dbdf2473aeb164aed7f44ae
samgcm <samuelcruz052@gmail.com>
2022-01-09 15:04:56 -0300

    Added new methods in class player, and main.

--- battleship/src/com/battleship/Main.java
+++ battleship/src/com/battleship/Main.java
@@ -3,19 +3,58 @@ package com.battleship;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        boolean keepPlay = true;
+        while(keepPlay) {
+            boolean choosenPlay = true;
+            boolean finishGame = true;
 
-        //As linha abaixo são só para teste das funcionalidades.
-        Player player = new Player();
-        Computer computer = new Computer();
-        player.showBoard();
-        player.putRandomSubmarines(99);
-        player.showBoard();
-        computer.putRandomSubmarines(1);
-        computer.showBoard();
-        System.out.println("");
-        System.out.println("LETS PLAY");
-        player.putBomb('E', 5, computer);
-        computer.putRandomBomb(player);
+            //As linha abaixo são só para teste das funcionalidades.
+            Player player = new Player();
+            Computer computer = new Computer();
+
+//        FASE DE POSICIONAMENTO DE NAVIOS
+            System.out.println("Você deseja colocar os submarinos de que forma?");
+            System.out.println("1 - Aleatória.\n2 - Escolher um por um.");
+            while(choosenPlay) {
+//            Verificar se o jogador escolheu o tipo de modo de colcoar os navios
+                boolean typePlay = player.howSetSubs();
+                if(typePlay) {
+                    computer.putRandomSubmarines(10);
+                    choosenPlay = false;
+                }
+            }
+
+            System.out.println("LETS PLAY");
+            while(finishGame){
+                if(player.getScore() == 0){
+                    computer.showBoard();
+                    System.out.printf("====    VENCEDOR    ====\n" +
+                                    "====    JOGADOR    ====\n" +
+                                    "==================\n" +
+                                    "====    PONTUAÇÃO    ====\n" +
+                                    "====   COMPUTADOR: %d    ====\n" +
+                                    "====    JOGADOR: %d    ===="
+                            , player.getScore(), computer.getScore());
+                    break;
+
+                } { player.setBomb(computer); }
+
+
+                if(computer.getScore() == 1) {
+                    computer.showBoard();
+                    System.out.printf(
+                            "====        VENCEDOR      ====\n" +
+                                    "====       COMPUTADOR     ====\n" +
+                                    "==============================\n" +
+                                    "====       PONTUAÇÃO      ====\n" +
+                                    "====    COMPUTADOR: %d    ====\n" +
+                                    "====       JOGADOR: %d    ====\n \n"
+                            , player.getScore(), computer.getScore());
+                    break;
+                } {computer.putRandomBomb(player);}
+            }
+
+            keepPlay = player.tryAgain();
+        }
     }
-
 }
